de7e40a6fc84ed79820dfa9d4270ef60703e2709
grconnor <connorroelofsen15@gmail.com>
2020-12-07 15:00:32 +0000

    Switch out build for main domain, and modify post build.

--- connorroelofsen_post_build/src/App.js
+++ connorroelofsen_post_build/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there Connor.
+          Placeholder
         </p>
       </header>
     </div>
